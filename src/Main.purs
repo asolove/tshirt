a809d2290262faa91732294e5aaa8e057c448707
Adam Solove <asolove@gmail.com>
2015-12-15 20:19:28 -0500

    First draw to the canvas.

--- src/Main.purs
+++ src/Main.purs
@@ -2,8 +2,18 @@ module Main where
 
 import Prelude
 import Control.Monad.Eff
-import Control.Monad.Eff.Console
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+import Data.Maybe.Unsafe (fromJust)
+
+import Graphics.Canvas (getCanvasElementById, getContext2D)
+import Graphics.Canvas.Free
+
 main = do
-  log "Hello, world!"
+  canvas <- getCanvasElementById "canvas"
+  context <- getContext2D $ fromJust canvas
+
+  runGraphics context $ do
+    -- Canvas API calls will go here
+    setFillStyle "#00FFFF"
+    rect { x: 0.0, y: 0.0, w: 400.0, h: 600.0 }
+    fill
